91c9b67301945e10ab3ca13de346a7d5138aca48
Gustavo <acuna.gustavo@gmail.com>
2018-01-27 14:49:50 -0300

    Te agrego la ruta gilastra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { ModuleWithProviders, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { DeliveryComponent } from './delivery/delivery.component';
 
 export const appRoutes: Routes = [
-  { path: 'users', component: UsersComponent}
+  { path: 'users', component: UsersComponent},
+  { path: 'delivery', component: DeliveryComponent}
 ];
